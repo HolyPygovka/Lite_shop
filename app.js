99f2269cdc3bb5b2f20804f8188c4cee45dfaf2f
Roman <r2598509@gmail.com>
2019-07-24 14:46:32 +0300

    mysql connection

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'market'
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+connection.query('SELECT * FROM goods', function(error, result, fields){
+    if(error) throw error;
+    console.log(result);
+});
+
+app.get('/', function (req, res) {
+  res.send('<p>Hello World!</p>');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
